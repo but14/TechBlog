7661300244602b04d52ab99a4c4c7b455fded00f
Longngoo <100961248+but14@users.noreply.github.com>
2025-01-13 15:11:58 +0700

    subscription, card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
+import { CategoryNavbarComponent } from "./layouts/category-navbar/category-navbar.component";
+import { FooterComponent } from "./layouts/footer/footer.component";
+import { PostCardComponent } from "./layouts/post-card/post-card.component";
+import { SubscriptionFormComponent } from "./subscription-form/subscription-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, PostCardComponent, SubscriptionFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
